8096c656d50f1b9a51b78a2b511372335571a43d
cefeon <cefeon@outlook.com>
2020-11-06 12:30:26 +0100

    Used clang-formatter to beautify code

--- hellothread.cpp
+++ hellothread.cpp
@@ -11,52 +11,54 @@ struct t_data
    double czesc;
 };
 
-double f (double x)
+double f(double x)
 {
    const double PI = 3.14159265358979323846;
    const double E = 2.718281828459045235360;
-   double func = pow(x,E*PI);
+   double func = pow(x, E * PI);
    return func;
 }
 
 double prostokatuj(double start, int i, double rozmiar)
 {
-   double dx = start + i*rozmiar;
-   double fx = f(dx+(rozmiar/2));
-   return fx*rozmiar;
+   double dx = start + i * rozmiar;
+   double fx = f(dx + (rozmiar / 2));
+   return fx * rozmiar;
 }
 
-void* worker(void* arg)
+void *worker(void *arg)
 {
-   struct t_data *args = (struct t_data*) arg;
+   struct t_data *args = (struct t_data *)arg;
    args->czesc = prostokatuj(args->start, args->licznik, args->rozmiar);
    printf("id=%lu result=%f \n", pthread_self(), args->czesc);
 }
 
 int main(int argc, char *argv[])
 {
-   if (argc != 4){
+   if (argc != 4)
+   {
       printf("\nWrong number of arguments. Should be: \n\n./hellothread start rozmiar N \n");
-   return 0;
+      return 0;
    }
-   
+
    int N = atoi(argv[3]);
    pthread_t tid[N];
    struct t_data args[N];
 
-   for (int i = 0; i < N; i++) {
+   for (int i = 0; i < N; i++)
+   {
       args[i].start = atof(argv[1]);
       args[i].rozmiar = atof(argv[2]);
       args[i].licznik = i;
       pthread_create(&tid[i], NULL, &worker, &args[i]);
    }
-   
+
    double wynik;
-   for (int i = 0; i < N; i++) {
-         pthread_join(tid[i], NULL);
-         wynik += args[i].czesc;
+   for (int i = 0; i < N; i++)
+   {
+      pthread_join(tid[i], NULL);
+      wynik += args[i].czesc;
    }
    printf("id=%lu result=%f \n", pthread_self(), wynik);
    pthread_exit(NULL);
 }
-
